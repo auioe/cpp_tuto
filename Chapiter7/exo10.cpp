f30968cba57a9b87b82a195b51ce1e5bd7207297
lingfeng <huanglf1996@gmail.com>
2024-04-05 05:38:04 +0800

    add func list solution to C7E10

--- Chapiter7/exo10.cpp
+++ Chapiter7/exo10.cpp
@@ -3,9 +3,10 @@
 
 double add(double, double);
 double multiply(double, double);
+double minus(double, double);
 double calculate(double, double, double (*op)(double, double));
 
-int main(){
+int main_1(){
   double a, b;
   std::cout << "Please enter 2 numbers, split by space: ";
   while(std::cin >> a >>b){
@@ -13,9 +14,21 @@ int main(){
     std::cout << "product of " << a << " and "<< b <<" is " << calculate(a, b, multiply) << std::endl;
     std::cout << "Next tuple: ";
   }
-  
+ return 1; 
 }
 
+int main(){
+  double a, b;
+  double (*fn[3])(double, double) = {add, multiply, minus};
+  std::cout << "Please enter 2 numbers, split by space: ";
+  while(std::cin >> a >>b){
+    for (int i=0; i<3; i++){
+      std::cout << calculate(a, b, fn[i]) << std::endl;
+    }
+    std::cout << "Next tuple: ";
+  }
+  
+}
 
 double add(double a, double b){
   return a + b;
@@ -25,4 +38,8 @@ double multiply(double a, double b){
 }
 double calculate(double a, double b, double (*op) (double, double)){
   return op(a, b);
+}
+
+double minus(double a, double b){
+  return a - b;
 }
